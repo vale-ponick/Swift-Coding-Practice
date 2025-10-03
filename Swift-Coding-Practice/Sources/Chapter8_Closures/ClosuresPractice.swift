dbaac3e9945c22196f0fa77573657be17b190e38
Valery Ponomareva <vale.ponick@gmail.com>
2025-10-03 12:55:02 +0700

    feat: Add examples of simple sorting closures for arrays

--- Swift-Coding-Practice/Sources/Chapter8_Closures/ClosuresPractice.swift
+++ Swift-Coding-Practice/Sources/Chapter8_Closures/ClosuresPractice.swift
@@ -178,7 +178,7 @@ public struct Chapter8_Closures {
      4. Специальная - стабильная, по ключу, кастомная логика
      
      5. По типу данных - для каждого типа свои особенности  */
-
+    
     static func task8a_2() {
         print("Задача 8a.2: Напиши 10 замыканий на примере сортировок массивов: Простая - по одному критерию.")
         
@@ -261,8 +261,99 @@ public struct Chapter8_Closures {
      10. По сумме цифр: [12, 21, 4, 33, 9, 77, 555, 888] */
     
     static func task8b_2() {
-        print("Зад...")
-    }
+        print("Задача 8a.2: Напиши 10 замыканий на примере сортировок массивов: Простая - по одному критерию.")
+        // ПРОСТАЯ сортировка - String
+        
+        let daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+        
+        let byLengthSorted = daysOfWeek.sorted { $0.count < $1.count }
+        print("1. Сортировка массива строк по длине: \(byLengthSorted)")
+        // 1. Сортировка массива строк по длине: ["Sunday", "Monday", "Friday", "Tuesday", "Thursday", "Saturday", "Wednesday"]
+        
+        let byAlphabetical = daysOfWeek.sorted()
+        print("2. Сортировка массива строк по алфавиту: \(byAlphabetical)") // 2. Сортировка массива строк по алфавиту: ["Friday", "Monday", "Saturday", "Sunday", "Thursday", "Tuesday", "Wednesday"]
+        
+        let byFirstChar = daysOfWeek.sorted {
+            let first1 = $0.first ?? Character("") //
+            let first2 = $1.first ?? Character("")
+            return first1 > first2
+        }
+        print("3. Сортировка массива строк по первому символу: \(byFirstChar)") // 3. Сортировка массива строк по первому символу: ["Wednesday", "Tuesday", "Thursday", "Sunday", "Saturday", "Monday", "Friday"]
+        
+        let byLastChar = daysOfWeek.sorted {
+            let last1 = $0.last ?? Character("") //
+            let last2 = $1.last ?? Character("")
+            return last1 > last2
+        }
+        print("4. Сортировка массива строк по последнему символу: \(byLastChar)") // 3. Сортировка массива строк по последнему символу: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+        
+        let byVowelsCount = daysOfWeek.sorted {
+            let vowels1 = $0.lowercased().filter {
+                "aeoiu".contains($0) }.count
+            let vowels2 = $1.lowercased().filter {
+                "aeoiu".contains($0) }.count
+            return vowels1 > vowels2
+        }
+        print("5. Сортировка массива строк по количеству гласных: \(byVowelsCount)") // 5. Сортировка массива строк по количеству гласных: ["Tuesday", "Wednesday", "Saturday", "Sunday", "Monday", "Thursday", "Friday"]
+        
+        let byLetter = daysOfWeek.sorted {
+            let hasI1 = $0.lowercased().contains("i")
+            let hasI2 = $1.lowercased().contains("i")
+            return hasI1 && !hasI2
+        }
+        print("6. Сортировка массива строк по определенной букве - 'i' - \(byLetter)") // 6. Сортировка массива строк по определенной букве - 'i' - ["Friday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Saturday"]
+        
+        let names = ["Joe", "ann", "Andy", "Boris", "bill", "Dennis", "denny"]
+        
+        let byRegister = names.sorted { $0 < $1 }
+        print("7. Сортировка массива строк с учетом регистра: \(byRegister)") // 7. Сортировка массива строк с учетом регистра: ["Andy", "Boris", "Dennis", "Joe", "ann", "bill", "denny"]
+        
+        let nums = [888, 12, 3, 45, 21, 777, 100, 99, 201]
+        let byIncreasing = nums.sorted { $0 < $1 }
+        print("8. Сортировка массива чисел по возрастанию: \(byIncreasing)") // 8. Сортировка массива чисел по возрастанию: [3, 12, 21, 45, 99, 100, 201, 777, 888]
+    
+    let byDecreasing = nums.sorted { $0 > $1 }
+    print("9. Сортировка массива чисел по убыванию: \(byDecreasing)") // 9. Сортировка массива чисел по убыванию: [888, 777, 201, 100, 99, 45, 21, 12, 3]
+        
+        let byEvenFirst = nums.sorted {
+            let num1 = $0 % 2 == 0
+            let num2 = $1 % 2 == 0
+            return num1 && !num2
+        }
+        print("10. Сортировка массива чисел: четные - вперед! \(byEvenFirst)") // 10. Сортировка массива чисел: четные - вперед! [888, 12, 100, 3, 45, 21, 777, 99, 201]
+        
+        let byOddFirst = nums.sorted {
+            let num1 = $0 % 2 != 0
+            let num2 = $1 % 2 != 0
+            return num1 && !num2
+        }
+        print("11. Сортировка массива чисел: НЕчетные - вперед! \(byOddFirst)") // 11. Сортировка массива чисел: НЕчетные - вперед! [3, 45, 21, 777, 99, 201, 888, 12, 100]
+        
+        let byDistanseFrom100 = nums.sorted {
+            abs($0 - 100) < abs($1 - 100)
+        }
+        print("Сортировка массива чисел 'ближайшее к 100' \(byDistanseFrom100)") // Сортировка массива чисел 'ближайшее к 100' [100, 99, 45, 21, 12, 3, 201, 777, 888]
+        
+        let byDigitSum = nums.sorted {
+            let sum1 = String($0).compactMap { $0.wholeNumberValue }.reduce(0, +)
+            let sum2 = String($1).compactMap { $0.wholeNumberValue }.reduce(0, +)
+            return sum1 < sum2
+        }
+        print("Сортировка массива чисел по суме цифр: \(byDigitSum)") // Сортировка массива чисел по суме цифр: [100, 12, 3, 21, 201, 45, 99, 777, 888]
+        
+        // 3. Упрощенная сортировка по первому символу
+        let byFirstCharacter = daysOfWeek.sorted { $0.first! > $1.first! }
+        // Или даже так (сравнение строк по первому символу):
+        // let byFirstChar = daysOfWeek.sorted { String($0.prefix(1)) > String($1.prefix(1)) }
+
+        // 4. Упрощенная сортировка по последнему символу
+        let byLastCharacter = daysOfWeek.sorted { $0.last! > $1.last! }
+
+        // 6. Упрощенная сортировка по наличию буквы 'i'
+        let byLetterI = daysOfWeek.sorted { $0.lowercased().contains("i") && !$1.lowercased().contains("i") }
+}
+  
+    
     
     static func task8c_2() {
         print("Зад...")
@@ -280,3 +371,4 @@ public struct Chapter8_Closures {
         print("Задача 8.5: Напиши функции, которые принимают в качестве аргументов массивы и словари и проверяют: пустые или нет. Если пустые - добавляют туда значения и выводят в консоль.")
     }
 }
+
